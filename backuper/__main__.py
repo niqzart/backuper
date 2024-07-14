5864cf502927a73f9f2fb3e1b803df8a46deb118
niqzart <qwert45hi@yandex.ru>
2024-07-14 12:48:41 +0300

    feat: parsing yaml to pydantic model

--- backuper/__main__.py
+++ backuper/__main__.py
@@ -1,11 +1,22 @@
 from typing import Annotated
 
+from pydantic import BaseModel, ValidationError
+from pydantic_yaml import parse_yaml_file_as
 from typer import Argument, FileText, run
 
 
-def main(config: Annotated[FileText, Argument(encoding="utf-8")]) -> None:
+class ConfigModel(BaseModel):
+    t: int
+
+
+def main(config_file: Annotated[FileText, Argument(encoding="utf-8")]) -> None:
     # TODO defaults for filename
-    print(type(config), config)  # noqa: T201 WPS421
+    try:
+        config = parse_yaml_file_as(ConfigModel, config_file)
+    except ValidationError as e:  # noqa: WPS329
+        raise e  # TODO error handling for parsing
+
+    print(config)  # noqa: T201 WPS421
 
 
 if __name__ == "__main__":
