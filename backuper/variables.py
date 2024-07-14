63edfdd07ffa7ea67f9084a79c5a5be6f152f338
niqzart <qwert45hi@yandex.ru>
2024-07-14 22:53:28 +0300

    feat: loading variables from environment

--- backuper/variables.py
+++ backuper/variables.py
@@ -1,3 +1,4 @@
+from os import getenv
 from string import Template
 from typing import Annotated
 
@@ -6,18 +7,27 @@ from pydantic import AfterValidator
 
 class VariableController:
     def __init__(self) -> None:
-        self.variables: dict[str, str] | None = None
+        self.variables: dict[str, str] = {}
 
-    def load_variables(self, variables: dict[str, str]) -> dict[str, str]:
-        self.variables = variables
-        return variables
+    def load_variable(self, key: str, value: str | None) -> str:
+        value = getenv(key, default=value)
+        if value is None:
+            raise EnvironmentError(f"Environment variable '{key}' should be specified")
+        return value
+
+    def load_variables(self, variables: dict[str, str | None]) -> dict[str, str]:
+        self.variables = {
+            key: self.load_variable(key=key, value=value)
+            for key, value in variables.items()
+        }
+        return self.variables
 
     def substitute(self, incoming_string: str) -> str:
         template = Template(incoming_string)
-        return template.substitute(self.variables or {})
+        return template.substitute(self.variables)
 
 
 vc = VariableController()
 
-Variables = Annotated[dict[str, str], AfterValidator(vc.load_variables)]
+Variables = Annotated[dict[str, str | None], AfterValidator(vc.load_variables)]
 SubstitutedStr = Annotated[str, AfterValidator(vc.substitute)]
