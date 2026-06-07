65bf3bca3fed8699fc53f49aed52d9e1abd9dd26
niqzart <qwert45hi@yandex.ru>
2026-06-07 06:10:34 +0300

    feat: rsync action for better control over remote backups

--- backuper/actions/remotes.py
+++ backuper/actions/remotes.py
@@ -1,3 +1,4 @@
+import re
 from collections.abc import Iterator
 from typing import Literal
 
@@ -28,8 +29,6 @@ class SCPLocationData(BaseModel):
 
 
 class SCPAction(SubShellAction):
-    # TODO switch to rsync to delete the source directory
-
     type: Literal["scp"]
     source: SCPLocationData
     target: SCPLocationData
@@ -84,3 +83,74 @@ class SSHAction(SubShellAction):
 
     def is_failed(self, return_code: int) -> bool:
         return return_code not in self.expected_exit_codes
+
+
+class RSyncLocationData(BaseModel):
+    username: SubstitutedStr | None = None
+    password: SubstitutedStr | None = None
+    host: SubstitutedStr | None = None
+    path: SubstitutedPath
+
+    @property
+    def path_for_rsync(self) -> str:
+        return re.sub("^([A-Z]):/", r"/cygdrive/\1/", self.path.as_posix(), count=1)
+
+    def build_argument(self) -> str:
+        if self.host is None:
+            return self.path_for_rsync
+        remote_location = (
+            AnyUrl.build(
+                scheme="rsync",
+                username=self.username,
+                password=self.password,
+                host=self.host,
+            )
+            .unicode_string()
+            .removeprefix("rsync://")
+        )
+        return f"{remote_location}:{self.path_for_rsync}"
+
+
+class RSyncBackupAction(SubShellAction):
+    type: Literal["rsync-backup"]
+
+    source: RSyncLocationData
+    target: RSyncLocationData
+
+    preserve_timestamps: bool = True
+    use_transport_compression: bool = True
+    override_partial_files: bool = False
+    bandwidth_limit: int | None = None  # KB/s
+    remote_shell: SubstitutedPath | None = None
+
+    # TODO: exclude / include
+
+    def collect_command(self) -> Iterator[str]:
+        yield "rsync"
+        yield "--recursive"
+        yield "--delete"
+        yield "--progress"
+
+        if self.preserve_timestamps:
+            yield "--times"
+
+        if self.use_transport_compression:
+            yield "--compress"
+
+        if not self.override_partial_files:
+            yield "--partial"
+
+        if self.bandwidth_limit is not None:
+            yield "--bwlimit"
+            yield str(self.bandwidth_limit)
+
+        if self.remote_shell is not None:
+            yield "--rsh"
+            yield self.remote_shell.as_posix()
+
+        yield self.source.build_argument()
+
+        yield self.target.build_argument()
+
+    def is_failed(self, return_code: int) -> bool:
+        return return_code != 0
